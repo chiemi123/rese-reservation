06b92e0231cece6b1d08c27d8ed776be8ee41215
chiemi endo <milkrose0315@yahoo.co.jp>
2025-05-21 20:43:07 +0900

    予約変更時のメール再送信を実装／QRコードをメールからリンク表示に変更／文面を予約変更用に切り替え

--- src/resources/views/emails/reservation.blade.php
+++ src/resources/views/emails/reservation.blade.php
@@ -1,12 +1,22 @@
 <p>{{ $userName }}さん、こんにちは！</p>
 
-<p>ご予約ありがとうございます。</p>
+@if ($isUpdate)
+    <p>予約内容の変更を承りました。</p>
+@else
+    <p>ご予約ありがとうございます。</p>
+@endif
+
+<p>以下が最新の予約内容です。</p>
 
 <p>予約日時：{{ $reservationTime }}</p>
 
-<p>このメールに添付されたQRコードを、店舗スタッフにご提示ください。</p>
+<p>下記の「QRコードを見る」ボタンをクリックし、表示されたQRコードを店舗スタッフにご提示ください。</p>
 
-<div>{!! $qrCodeHtml !!}</div>
+
+<a href="{{ $qrUrl }}"
+    style="display: inline-block; padding: 10px 20px; background-color: #3490dc; color: #fff; text-decoration: none; border-radius: 5px;">
+    QRコードを見る
+</a>
 
 <p>ご不明な点がございましたら、いつでもお問い合わせください。</p>
 
